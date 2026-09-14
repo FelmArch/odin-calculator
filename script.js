cf5d511ba595f4a925d4624900cb85f4fc4400fb
FelmArch <ferrlinda.dl@gmail.com>
2026-09-14 08:51:07 +0200

    feat: adicionar funcao operate para despachar operacoes matematicas

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function multiplicar(a, b) {
 function dividir(a, b) {
     return a / b;
 }
+
+function operate(operador, a, b) {
+    // Primeiro, verificamos: o operador é "+"?
+    if (operador === "+") {
+        return adicionar(a, b);
+
+    // Se não é "+", será que é "-"?
+    } else if (operador === "-") {
+        return subtrair(a, b);
+
+    // Se não é "-", será que é "*"?
+    } else if (operador === "*") {
+        return multiplicar(a, b);
+
+    // Se não é "*", será que é "/"?
+    } else if (operador === "/") {
+        return dividir(a, b);
+    }
+}
